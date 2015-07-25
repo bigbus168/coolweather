44b7765821111d72c265b14f3e3142d860a57e56
Edison <675054453@qq.com>
2015-07-25 15:20:41 +0800

    加入显示天气信息的功能。

--- src/db/CoolWeatherDB.java
+++ src/db/CoolWeatherDB.java
@@ -129,7 +129,7 @@ public class CoolWeatherDB {
 		if(county != null){
 			ContentValues values = new ContentValues();
 			values.put("county_name", county.getCountyName());
-			values.put("count_code", county.getCountyCode());
+			values.put("county_code", county.getCountyCode());
 			values.put("city_id", county.getCityId());
 			db.insert("County", null, values);
 		}
